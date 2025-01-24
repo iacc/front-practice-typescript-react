88cba424c1ee46da89d598aa239278341c95cf7b
yamagata-satoshi-z <satoshi.yamagata@zigexn.co.jp>
2025-01-24 15:17:18 +0900

    react6回答

--- react/src/practice/practice_6/index.tsx
+++ react/src/practice/practice_6/index.tsx
@@ -1,10 +1,34 @@
 // 問題1: useStateを使ったカウンター
 // 次の条件を満たすカウンターコンポーネントを作成してください：
 
+import { useState } from 'react';
+
 // 初期値を0に設定します。
 // 「増加」「減少」「リセット」の3つのボタンを追加します。
 // ボタンをクリックすると、それぞれの機能が正しく動作します。
 
 export const Practice6 = () => {
-  return <>{/* コードをここに書いてください */}</>;
-}
+  const start = 0;
+  const [num, countNum] = useState<number>(start);
+  const increment = () => countNum(num + 1);
+  const decrement = () => countNum(num - 1);
+  const reset = () => countNum(start);
+  return (
+    <>
+      {num}
+      <ul>
+        <li>
+          <button onClick={increment}>+1</button>
+        </li>
+
+        <li>
+          <button onClick={decrement}>-1</button>
+        </li>
+
+        <li>
+          <button onClick={reset}>reset</button>
+        </li>
+      </ul>
+    </>
+  );
+};
