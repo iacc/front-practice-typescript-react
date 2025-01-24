2f0b43e19c648ef0bd6a9f8ce12257178abb7cd5
yamamoto-ken-z <ken.yamamoto@zigexn.co.jp>
2025-01-24 12:58:55 +0900

    AM practice typescript

--- typescript/src/practice_2/index.ts
+++ typescript/src/practice_2/index.ts
@@ -1,10 +1,17 @@
 // 演習2: 以下の関数に型を追加してください。
 
 // 以下のコメントを外して問題を解く
-// function add(a, b) {
-//   return a + b;
-// }
+function add(a: number, b: number): number {
+  return a + b;
+}
 
-// function formatId(id) {
+
+
+function formatId(id: string): string {
+  return `ID: ${id}`;
+}
+
+// これもいける
+// function formatId(id: number): string {
 //   return `ID: ${id}`;
 // }
